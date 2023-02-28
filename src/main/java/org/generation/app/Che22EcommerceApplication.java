9088e03bc684b649500a124bff7326269a27347a
Brayan Correa <brayan_seyer@hotmail.com>
2023-02-28 13:12:31 -0600

    feat:inicio del proyecto SpringBoot

---
+++ src/main/java/org/generation/app/Che22EcommerceApplication.java
@@ -0,0 +1,13 @@
+package org.generation.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Che22EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Che22EcommerceApplication.class, args);
+	}
+
+}
